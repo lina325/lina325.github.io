f149d2e481fca9c8a55091742141161f2d0cd8f7
lina325 <2402749@spsd.sk.ca>
2024-09-19 11:03:18 -0600

    Put in interactive scene code
    Add images from p5js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,17 +1,62 @@
 // Project Title
-// Your Name
-// Date
+// Angelina Zhu
+// Sept __, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+// "Click on the canvas to begin detecting key presses" - From the reference lol
+
+let x = 200;
+let y = 200;
+let bgColour = 220;
+
+let hinataImg;
+let kagsImg;
+let kenmaImg;
+let kurooImg;
+let characterList = [hinataImg, kagsImg, kenmaImg, kurooImg];
+let n = 0;
+let character = characterList[n];
+
+
+function preload() {
+  hinataImg = loadImage('hinataCrow.png');
+  kagsImg = loadImage('kagsCrow.png');
+  kenmaImg = loadImage('kenmaCat.png');
+  kurooImg = loadImage('kurooCat.png');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-
-  circle(mouseX, mouseY, 30);
+  background(bgcolour);
+  
+  // showCharacter();
 }
+
+// function keyPressed() {
+//   if (key === '1') { 
+//     bgcolour = ('rgb(16,23,114)')
+//   }
+//   else if (key === '2') {
+//     bgcolour = ('rgb(236,0,0)')
+//   }
+//   // else if (key === '3') {}
+  // else if (key === '4') {}
+  // else if (key === '5') {}
+  // else if (key === '6') {}
+  // else if (key === '7') {}
+// } 
+
+function mousePressed(){
+  character = characterList[n+1]; 
+  console.log(character)
+}
+
+// function showCharacter() {
+//   image(character, x - img.width, y - img.height)
+// }
