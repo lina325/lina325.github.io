6e42d73c54db5bc2d518e2a61eaa409814c2188a
lina325 <2402749@spsd.sk.ca>
2024-09-20 11:02:37 -0600

    Troubleshooting
    Still trying to figure out the images

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,27 +6,27 @@
 // - describe what you did to take this project "above and beyond"
 
 
-// "Click on the canvas to begin detecting key presses" - From the reference lol
+// Click on the canvas to begin detecting key presses
 
 let x = 200;
 let y = 200;
-let bgColour = 220;
+let bgcolour = 220;
 
-let hinataImg;
-let kagsImg;
-let kenmaImg;
-let kurooImg;
-let characterList = [hinataImg, kagsImg, kenmaImg, kurooImg];
-let n = 0;
-let character = characterList[n];
+// let hinataImg;
+// let kagsImg;
+// let kenmaImg;
+// let kurooImg;
+// // let characterList = ['hinataImg', 'kagsImg', 'kenmaImg', 'kurooImg'];
+// let n = 0;
+// let character = characterList[n];
 
 
-function preload() {
-  hinataImg = loadImage('hinataCrow.png');
-  kagsImg = loadImage('kagsCrow.png');
-  kenmaImg = loadImage('kenmaCat.png');
-  kurooImg = loadImage('kurooCat.png');
-}
+// function preload() {
+  // hinataImg = loadImage('hinataCrow.png');
+  // kagsImg = loadImage('kagsCrow.png');
+  // kenmaImg = loadImage('kenmaCat.png');
+  // kurooImg = loadImage('kurooCat.png');
+// }
 
 function setup() {
   createCanvas(400, 400);
@@ -35,28 +35,32 @@ function setup() {
 function draw() {
   background(bgcolour);
   
+  square(x, y, 50);
   // showCharacter();
 }
 
-// function keyPressed() {
-//   if (key === '1') { 
-//     bgcolour = ('rgb(16,23,114)')
-//   }
-//   else if (key === '2') {
-//     bgcolour = ('rgb(236,0,0)')
-//   }
-//   // else if (key === '3') {}
-  // else if (key === '4') {}
+function keyPressed() {
+  if (key === '1') { 
+    bgcolour = 'rgb(16,23,114)'
+  }
+  else if (key === '2') {
+    bgcolour = 'rgb(236,0,0)'
+  }
+  else if (key === '3') {
+    bgcolour = 'white'
+  }
+  else if (key === '4') {
+    bgcolour = 'black'
+  }
   // else if (key === '5') {}
   // else if (key === '6') {}
   // else if (key === '7') {}
-// } 
+} 
 
-function mousePressed(){
-  character = characterList[n+1]; 
-  console.log(character)
-}
+// function mousePressed(){
+//   character = characterList[n+1]; 
+// }
 
 // function showCharacter() {
-//   image(character, x - img.width, y - img.height)
+//   image(character, x, y);
 // }
