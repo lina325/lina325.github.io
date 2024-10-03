22b8a9792ea172dfd91336eb4be69f5d14bfdb2c
lina325 <2402749@spsd.sk.ca>
2024-10-03 11:01:04 -0600

    bouncing ball demo added

---
+++ bouncing-balls/sketch.js
@@ -0,0 +1,49 @@
+// Bouncing Balls 
+// October 3, 2024 (Happy Channie dayyyyyy)
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  for (let theBall of ballArray) {
+    // Move the ball
+    theBall.x += theBall.dx;
+    theBall.y += theBall.dy;
+
+    // Bounce if needed
+    if (theBall.x > width - theBall.radius || theBall.x < 0 + theBall.radius) {
+      theBall.dx *= -1;
+    }
+    if (theBall.y > height - theBall.radius || theBall.y < 0 + theBall.radius) {
+      theBall.dy *= -1;
+    }
+
+    // Show the ball
+    noStroke();
+    fill(theBall.red, theBall.green, theBall.blue);
+    circle(theBall.x, theBall.y, theBall.radius*2);
+  }
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function spawnBall(theX, theY) {
+  let theBall = {
+    x: theX, 
+    y: theY, 
+    radius: random(30, 60),
+    dx: random(-5, 5), 
+    dy: random(-5, 5),
+    red: random(255), 
+    green: random(255), 
+    blue: random(255),
+  };
+  ballArray.push(theBall);
+}
