9d6d185b3a6236bf048f942b51ea60eae889909e
lina325 <2402749@spsd.sk.ca>
2024-10-09 11:03:11 -0600

    Started work on background in array/objects project

--- arrays-and-objects/sketch.js
+++ arrays-and-objects/sketch.js
@@ -5,28 +5,39 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rects = [];
-const NUM_OF_RECTS = 2000;
+let hills = [];
+let someRect;
+const WIDTH_OF_RECTS = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let widthOfRects = width/NUM_OF_RECTS;
+  makeHills(WIDTH_OF_RECTS);
 }
 
 function draw() {
   background(220);
 
-  for (let i = 0; i < width; i++) { // How to make it continuous...?
-
+  for (let theRect of hills) {
+    rect(theRect.x, theRect.y, theRect.width, theRect.height);
   }
+
+  moveCharacter();
+  spawnCharacter();
 }
 
-function makeHills(widthOfRects) {
+function makeHills(WIDTH_OF_RECTS) {
   let time = 0;
   let deltaTime = 0.001;
 
-  for (let x = 0; x < width; x += widthOfRects) {
-    
+  // Styling 
+  noStroke;
+  fill("green");
+
+  for (let x = 0; x < width; x += WIDTH_OF_RECTS) {
+    let theHeight = noise(time) * height;
+    someRect = makeRect(x, theHeight, WIDTH_OF_RECTS);
+    hills.push(someRect);
+    time += deltaTime;
   }
 }
 
@@ -38,4 +49,25 @@ function makeRect(xPos, rectHeight, rectWidth) {
     height: rectHeight,
   };
   return aRect;
+}
+
+function spawnCharacter() {
+  let character = {
+    charX: 0,
+    charY: height - hills[0].height,
+    charw: 50,
+  };
+  
+  square(character.charX, character.charY, character.width);
+}
+
+function moveCharacter() {
+  if (keyIsPressed) {
+    if (key === RIGHT_ARROW) {
+      character.x += 2;
+    }
+    else if (key === LEFT_ARROW) {
+      character.x -= 2;
+    }
+  }
 }
