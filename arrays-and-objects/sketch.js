a8661c5193f1e5b384b69c8b09dd262126d5d46d
lina325 <2402749@spsd.sk.ca>
2024-10-14 23:52:30 -0600

    "character" moving, working on bg

--- arrays-and-objects/sketch.js
+++ arrays-and-objects/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let hills = [];
+let hills1 = [];
 let someRect;
 const WIDTH_OF_RECTS = 2;
 
@@ -15,35 +16,66 @@ let character;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   makeHills(WIDTH_OF_RECTS);
+  makeHills1(WIDTH_OF_RECTS);
+  setCharacter();
+
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  background("#b7c4f7");
 
+  displayHills1();
   displayHills();
 
-  // moveCharacter();
   // characterMotion();
   displayCharacter();
 }
 
 function displayHills() {
   // Styling
-  noStroke();
   fill("#035718");
 
-  for (let theRect of hills) {
-    rect(theRect.x, theRect.y, theRect.width, theRect.height);
+  // for (let theRect of hills) {
+  //   rect(theRect.x, theRect.y, theRect.width, theRect.height);
+  // }
+
+  // Maybe a nested loops will work?
+  for (let i = 0; i < width; i++) {
+    rect(hills[i].x, hills[i].y, hills[i].width, hills[i].height);
   }
+}
+
+function moveHills() {
+  
 }
 
 function makeHills(WIDTH_OF_RECTS) {
   let time = 0;
-  let deltaTime = 0.0007; // Look for numbers to make hills less drastic, maybe 0.0003 - 0.0006
+  let deltaTime = 0.0004; // Look for numbers to make hills less drastic, maybe 0.0003 - 0.0006
+
+  for (let x = 0; x < width + 500; x += WIDTH_OF_RECTS) {
+    let theHeight = noise(time) * (height/2);
+    hills.push(makeRect(x, theHeight, WIDTH_OF_RECTS));
+    time += deltaTime;
+  }
+}
+
+function displayHills1() {
+  fill("#01260a");
+
+  for (let theRect of hills1) {
+    rect(theRect.x, theRect.y, theRect.width, theRect.height);
+  }
+}
+
+function makeHills1(WIDTH_OF_RECTS) {
+  let time = 5;
+  let deltaTime = 0.003; // Look for numbers to make hills less drastic, maybe 0.0003 - 0.0006
 
   for (let x = 0; x < width; x += WIDTH_OF_RECTS) {
     let theHeight = noise(time) * height;
-    makeRect(x, theHeight, WIDTH_OF_RECTS);
+    hills1.push(makeRect(x, theHeight, WIDTH_OF_RECTS));
     time += deltaTime;
   }
 }
@@ -55,31 +87,26 @@ function makeRect(xPos, rectHeight, rectWidth) {
     width: rectWidth,
     height: rectHeight,
   };
-  hills.push(aRect);
+  return aRect;
 }
 
-function displayCharacter() {
+function setCharacter() {
   character = {
     charW: 50,
     charX: 0,
-    charY: height, // need to access rectHeight to match the height of the rect each time...
+    charY: height - hills[0].height, // Take away image height
   };
-  
-  fill(0);
-  square(character.charX, character.charY, character.charW);
 }
 
-// function characterMotion() {
-//   character.charX += 2;
-// }
+function displayCharacter() {
+  fill(0);
+  square(character.charX, character.charY, character.charW);  
+}
 
-// function moveCharacter() {
-//   if (keyIsPressed) {
-//     if (key === RIGHT_ARROW) {
-//       character.charX += 2;
-//     }
-//     else if (key === LEFT_ARROW) {
-//       character.charX -= 2;
-//     }
-//   }
-// }
+function characterMotion() {
+  character.charX += 2;
+
+  // for (let i = 1; i < width; i ++) {
+  character.charY = height - hills[1].height;
+  // }
+}
