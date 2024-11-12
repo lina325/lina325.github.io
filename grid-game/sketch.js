d410cb8b13e96ddd8c00e8f7d93b8a5325b08f50
lina325 <2402749@spsd.sk.ca>
2024-11-11 21:18:38 -0600

    Check system working
    Working on visuals/e4e

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -13,11 +13,20 @@ let turn = 0;
 let playerTurn = 0;
 
 let screenState = "start";
+let winColour;
+let win = false;
+let allFilled = false;
+let circleMask;
+let blueChip;
 
 let buttonProperties = {
   width: 300, 
   height: 110,
 };
+
+function preload() {
+  blueChip = loadImage("blue-chip.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -37,13 +46,16 @@ function draw() {
     displayStartScreen();
   }
   else if (screenState === "player-player") {
-    background(100); //#fcf8eb
+    background(100); //#fcf8eb Maybe change
 
     displayBoard();
     checkWin();
   }
   else if (screenState === "win") {
     announceWinner(winColour);
+  }
+  else if (screenState === "tie") {
+    tie();
   }
 }
 
@@ -61,7 +73,7 @@ function generateGrid(cols, rows) {     // Center it?
 }
 
 function displayStartScreen() {
-  background(240);
+  background(240); //Maybe add/replace with image?
 
   fill(0);
   textAlign(CENTER);
@@ -77,12 +89,19 @@ function displayStartScreen() {
 }
 
 function displayBoard() {
+  noStroke();
+
   for (let x = 0; x < NUM_OF_COLS; x ++) {
     for (let y = 0; y < NUM_OF_ROWS; y ++) {
       fill(grid[x][y].colour);
       circle(x * squareSize + squareSize/2, y * squareSize + squareSize/2, grid[x][y].diameter);
     }
   }
+
+  // Buttons to play again or back to start
+  // fill(255);
+  // stroke(2);
+  // rect(width/2 - buttonProperties.width/2, (height/3)*2 - buttonProperties.height, buttonProperties.width, buttonProperties.height);
 }
 
 function mouseClicked() {
@@ -93,7 +112,7 @@ function mouseClicked() {
       for (let y = NUM_OF_ROWS - 1; y >= 0; y --) {
         if (grid[x][y].state === "empty") {
           grid[x][y].colour = "#d10000"; 
-          playerTurn ++;    // May be better way to do this without putting it twice
+          playerTurn ++;
           break;
         }
       }
@@ -120,9 +139,6 @@ function mouseClicked() {
 }
 
 function checkWin() {
-  let win = "false";
-  let winColour;
-
   for (let x = 0; x < NUM_OF_COLS; x ++) {
     for (let y = 0; y < NUM_OF_ROWS; y ++) {
       
@@ -141,11 +157,11 @@ function checkWin() {
       // Check horizontal 
       if (x+3 < NUM_OF_COLS) {
         if (grid[x][y].colour === "#d10000" && grid[x+1][y].colour === "#d10000" && grid[x+2][y].colour === "#d10000" && grid[x+3][y].colour === "#d10000") {
-          winColour = grid[x][y].colour;
+          winColour = "Red";
           win = true;
         } 
         else if (grid[x][y].colour === "#1100a6" && grid[x+1][y].colour === "#1100a6" && grid[x+2][y].colour === "#1100a6" && grid[x+3][y].colour === "#1100a6") {
-          winColour = grid[x][y].colour;
+          winColour = "Blue";
           win = true;
         } 
       }
@@ -163,12 +179,12 @@ function checkWin() {
       }
 
       // Check right to left diagonal
-      if (x-3 < NUM_OF_COLS && y-3 < NUM_OF_ROWS) {
-        if (grid[x][y].colour === "#d10000" && grid[x-1][y-1].colour === "#d10000" && grid[x-2][y-2].colour === "#d10000" && grid[x-3][y-3].colour === "#d10000") {
+      if (x-3 > NUM_OF_COLS && y+3 < NUM_OF_ROWS) {
+        if (grid[x][y].colour === "#d10000" && grid[x-1][y+1].colour === "#d10000" && grid[x-2][y+2].colour === "#d10000" && grid[x-3][y+3].colour === "#d10000") {
           winColour = "Red";
           win = true;
         } 
-        else if (grid[x][y].colour === "#1100a6" && grid[x-1][y-1].colour === "#1100a6" && grid[x-2][y-2].colour === "#1100a6" && grid[x-3][y-3].colour === "#1100a6") {
+        else if (grid[x][y].colour === "#1100a6" && grid[x-1][y+1].colour === "#1100a6" && grid[x-2][y+2].colour === "#1100a6" && grid[x-3][y+3].colour === "#1100a6") {
           winColour = "Blue";
           win = true;
         } 
@@ -176,26 +192,43 @@ function checkWin() {
     }
   }
 
+  // Check for tie
+  for (let x = 0; x < NUM_OF_COLS; x ++) {
+    for (let y = 0; y < NUM_OF_ROWS; y ++) {
+      if (grid[x][y].state === "empty") {
+        return false;
+      }
+    }
+  }
+  allFilled = true;
+
   if (win === true) {
     screenState = "win";
+  }
+  else if (win !== true && allFilled) {
+    screenState = "tie"
   }
 }
 
 function announceWinner(winColour) {
   fill(0);
   textAlign(CENTER);
-  textSize(200);
-  text(`${winColour} won!`, NUM_OF_COLS*squareSize/2, NUM_OF_ROWS*squareSize/2); 
+  textSize(75);
+  text(`${winColour} wins!`, width - (width - NUM_OF_COLS*squareSize)/2, height/3); 
+}
 
-  rect(width/2 - buttonProperties.width);
-  // Buttons to play again or back to start
+function tie() {
+  fill(0);
+  textAlign(CENTER);
+  textSize(60);
+  text("You both lost, \nwomp womp.", width - (width - NUM_OF_COLS*squareSize)/2, height/3); 
 }
 
 function chipProperties(x, y) {
   let chip = {
     x: x, 
     y: y,
-    diameter: squareSize - 25, 
+    diameter: squareSize - squareSize*0.2, 
     colour: 255,
     state: "empty",
   };
